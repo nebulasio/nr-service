df37f1e646ee06eeffb7ed9101dd68a5a32a7114
chmwang <chmwang@zju.edu.cn>
2018-08-24 20:06:14 +0800

    rest api

---
+++ python/api/api.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python2.7
+'''
+Author: Chenmin Wang
+Date: 2018/08/24
+'''
+
+import sys
+sys.path.append('../')
+
+from flask import Flask, url_for, request, json
+from flask_restful import Resource, Api
+
+import db_reader
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/nr')
+def api_nr():
+    l = list()
+    if 'start_block' in request.args and 'end_block' in request.args:
+        start_block = int(request.args['start_block'])
+        end_block = int(request.args['end_block'])
+        l = db_reader.read_from_transaction_with_duration(start_block, end_block)
+    return l
+
+
+@app.route('/hello')
+def api_hello():
+    return 'hello\n'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='6000', debug=True)
---
+++ python/api/db_reader.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/pydoc2.7
+'''
+Author: Chenmin Wang
+Date: 2018/08/24
+'''
+
+import sys
+sys.path.append('../')
+import json
+
+from graph.arango_graph import database
+
+
+def read_from_transaction_with_duration(start_block, end_block):
+    '''
+    @start_block - block height start
+    @end_block - block height end
+    @return - time spend
+    '''
+
+    assert isinstance(start_block, int)
+    assert isinstance(end_block, int)
+
+    aql = '''for tx in transaction filter tx.height>=%d and tx.height<=%d
+            return tx''' % (start_block, end_block)
+
+    cursor = database.aql.execute(aql, count=True, batch_size=0x7fffffff)
+    result = [doc for doc in cursor]
+    return json.dumps(result)
+
+
+def main():
+    start_block = int(sys.argv[1])
+    end_block = int(sys.argv[2])
+
+    l = read_from_transaction_with_duration(start_block, end_block)
+    print l
+
+
+if __name__ == '__main__':
+    main()
