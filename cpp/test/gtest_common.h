cf16953ef39a5252a72e06b60eb16c32993bc176
chmwang <chmwang@zju.edu.cn>
2018-08-31 16:31:26 +0800

    gtest for account db append

---
+++ cpp/test/gtest_common.h
@@ -0,0 +1,20 @@
+#include "blockchain.h"
+#include "graph.h"
+
+typedef typename boost::graph_traits<
+    neb::transaction_graph::internal_graph_t>::vertex_iterator viterator_t;
+typedef typename boost::graph_traits<
+    neb::transaction_graph::internal_graph_t>::out_edge_iterator oeiterator_t;
+
+typedef std::shared_ptr<neb::transaction_db_interface> transaction_ptr_t;
+typedef neb::transaction_db<neb::nebulas_db> transaction_db_t;
+
+typedef std::shared_ptr<neb::account_db_interface> account_ptr_t;
+typedef neb::nebulas::nebulas_account_db nebulas_account_db_t;
+
+extern transaction_db_t nt_db;
+extern transaction_ptr_t transaction_db_ptr;
+
+extern nebulas_account_db_t na_db;
+extern account_ptr_t account_db_ptr;
+
---
+++ cpp/test/gtest_nebulas_account_db.cpp
@@ -0,0 +1,35 @@
+#include "blockchain.h"
+#include "gtest_common.h"
+#include "utils.h"
+#include <gtest/gtest.h>
+
+TEST(test_nebulas_account_db, test_append_account_to_db_full) {
+  nebulas_account_db_t na_db(STR(DB_URL), STR(DB_USER_NAME), STR(DB_PASSWORD),
+                             STR(NEBULAS_DB));
+  account_ptr_t account_db_ptr = std::make_shared<nebulas_account_db_t>(na_db);
+
+  std::vector<neb::account_info_t> rs = account_db_ptr->read_account_from_db();
+  for (auto it = rs.begin(); it != rs.end(); it++) {
+    std::string address = it->template get<::neb::address>();
+    neb::block_height_t height = it->template get<::neb::height>();
+    std::string balance = it->template get<::neb::balance>();
+    std::string account_type = it->template get<::neb::account_type>();
+    LOG(INFO) << address << ',' << height << ',' << balance << ','
+              << account_type;
+
+    std::pair<std::string, int> account_state =
+        neb::nebulas::get_account_state(height, address);
+
+    EXPECT_TRUE(balance.compare(account_state.first) == 0);
+    if (account_type.compare("normal") == 0) {
+      EXPECT_TRUE(account_state.second == 0x57);
+    } else if (account_type.compare("contract") == 0) {
+      EXPECT_TRUE(account_state.second == 0x58);
+    }
+
+    if (it - rs.begin() > 1000) {
+      break;
+    }
+  }
+}
+
