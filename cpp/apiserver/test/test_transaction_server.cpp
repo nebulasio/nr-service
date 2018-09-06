154e3418c5e876d66d550b0b70415916f687e3d7
Xuepeng Fan <athrunarthur@gmail.com>
2018-09-06 21:19:40 +0800

    add test

---
+++ cpp/apiserver/test/test_transaction_server.cpp
@@ -0,0 +1,11 @@
+#include "api/transaction_apiserver.h"
+
+int main(int argc, char *argv[]) {
+
+  transaction_apiserver server("server");
+
+  std::unordered_map<std::string, std::string> params = {
+      {"start_block", "400000"}, {"end_block", "400600"}};
+  server.on_api_transaction(params);
+  return 0;
+}
