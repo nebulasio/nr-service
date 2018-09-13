19a7c825a3a184d8384d302f3677a0e3f563e8ae
chmwang <chmwang@zju.edu.cn>
2018-09-13 20:34:27 +0800

    unittest for eth transaction

--- cpp/test/gtest_common.h
+++ cpp/test/gtest_common.h
@@ -8,11 +8,13 @@ typedef typename boost::graph_traits<
 
 typedef std::shared_ptr<neb::transaction_db_interface> transaction_ptr_t;
 typedef neb::transaction_db<neb::nebulas_db> transaction_db_t;
+typedef neb::transaction_db<neb::eth_db> eth_transaction_db_t;
 
 typedef std::shared_ptr<neb::account_db_interface> account_ptr_t;
 typedef neb::nebulas::nebulas_account_db nebulas_account_db_t;
 
 extern transaction_db_t nt_db;
+extern eth_transaction_db_t et_db;
 extern transaction_ptr_t transaction_db_ptr;
 
 extern nebulas_account_db_t na_db;
