b8e61459771d5ffbcd1904722dc7d025293146ff
chmwang <chmwang@zju.edu.cn>
2018-10-04 10:15:05 +0800

    arangodb env var

--- cpp/env.set.sh
+++ cpp/env.set.sh
@@ -5,3 +5,10 @@ export CC=$CUR_DIR/lib/bin/clang
 export CXX=$CUR_DIR/lib/bin/clang++
 export PATH=$CUR_DIR/lib/bin:$PATH
 export LD_LIBRARY_PATH=$CUR_DIR/lib/lib:$LD_LIBRARY_PATH
+
+# arangodb url, user, passwd, db
+export DB_URL=
+export DB_USER_NAME=
+export DB_PASSWORD=
+export NEBULAS_DB=
+
